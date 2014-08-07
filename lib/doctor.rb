5891e5e90ce4aea3a7f3ae2831191b34e2785783
Matt L Waechter & Andrew M Westling <student@epicodus.com>
2014-08-07 10:37:17 -0700

    add save and .all methods to Doctor

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -8,4 +8,24 @@ class Doctor
     @specialty_id = attributes[:specialty_id]
   end
 
+  def self.all
+    from_db = DB.exec("SELECT * FROM doctors")
+    all_doctors = []
+    from_db.each do |doctor|
+      name = doctor['name']
+      insurance_id = doctor['insurance_id']
+      specialty_id = doctor['specialty_id']
+      all_doctors << Doctor.new({:name => name, :insurance_id => insurance_id, :specialty_id => specialty_id})
+    end
+    all_doctors
+  end
+
+  def save
+    DB.exec("INSERT INTO doctors (name, insurance_id, specialty_id) VALUES ('#{name}', #{insurance_id}, #{specialty_id});")
+  end
+
+  def ==(another_doctor)
+    self.name == another_doctor.name
+  end
+
 end
--- spec/doctor_spec.rb
+++ spec/doctor_spec.rb
@@ -3,8 +3,33 @@ require 'doctor'
 
 describe "Doctor" do
   it 'is initialized with a name and specialty' do
-    test_doctor = Doctor.new({:name => 'Dr. Smith', :insurance => "Heart"})
+    test_doctor = Doctor.new({:name => 'Dr. Smith', :insurance_id => 1})
     expect(test_doctor).to be_an_instance_of Doctor
   end
 
+  describe "save" do
+    it "saves Doctor.new data to the database" do
+      test_doctor = Doctor.new({:name => 'Dr. Smith', :insurance_id => 1, :specialty_id => 1})
+      test_doctor.save
+      expect(Doctor.all).to eq [test_doctor]
+    end
+  end
+
+  describe ".all" do
+    it "returns all doctors when Doctor.all is called" do
+      test_doctor1 = Doctor.new({:name => 'Dr. Smith', :insurance_id => 1, :specialty_id => 1})
+      test_doctor2 = Doctor.new({:name => 'Dr. Johnson', :insurance_id => 1, :specialty_id => 1})
+      test_doctor1.save
+      test_doctor2.save
+      expect(Doctor.all).to eq [test_doctor1, test_doctor2]
+    end
+  end
+
+  describe "==" do
+    it "two doctors with the same name are the same doctor" do
+      test_doctor1 = Doctor.new({:name => 'Dr. Smith', :insurance_id => 1, :specialty_id => 1})
+      test_doctor2 = Doctor.new({:name => 'Dr. Smith', :insurance_id => 1, :specialty_id => 1})
+      expect(test_doctor1).to eq test_doctor2
+    end
+  end
 end
